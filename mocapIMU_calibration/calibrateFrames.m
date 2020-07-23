f5f133a1d46ff1428eef564db56ec3bcc70d1ca6
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2020-07-23 17:52:15 -0400

    Add alignFrames() to replace the manual process of aligning frames.

--- mocapIMU_calibration/calibrateFrames.m
+++ mocapIMU_calibration/calibrateFrames.m
@@ -1,4 +1,4 @@
-function [C_sm, costFuncHist] = calibrateFrames(syncedData, phi, TOL)
+function [C_sm, costFuncHist] = calibrateFrames(dataSynced, phi, TOL)
 % Find the DCM C_sm representing the rotation between the sensor frame of the 
 % IMU (F_s) and the body frame assigned by the Mocap system (F_m).
 % Requires the complexStepJacobianLie code from decar_utils.
@@ -13,21 +13,21 @@ function [C_sm, costFuncHist] = calibrateFrames(syncedData, phi, TOL)
         TOL = 1E-4; % default nonlinear least squares convergence tolerance 
     end
     
-    numAcc  = length(syncedData.accIMU);   % number of accel meas
-    numGyr  = length(syncedData.omegaIMU); % number of gyr meas
+    numAcc  = length(dataSynced.accIMU);   % number of accel meas
+    numGyr  = length(dataSynced.omegaIMU); % number of gyr meas
     delta = Inf;
 
     %% LS Optimization on SO(3), using Gauss-Newton
     costFuncHist = [];
     C_sm = ROTVEC_TO_DCM(phi);
     f = @(C) computeErrorVector(C,...
-                                syncedData.accMocap, syncedData.omegaMocap,...
-                                syncedData.accIMU,   syncedData.omegaIMU);
+                                dataSynced.accMocap, dataSynced.omegaMocap,...
+                                dataSynced.accIMU,   dataSynced.omegaIMU);
     while norm(delta) > TOL
         e = f(C_sm);
         A = complexStepJacobianLie(f,C_sm,3,@CrossOperator);
         
-        costFuncHist = [costFuncHist; 0.5*e'*e];
+        costFuncHist = [costFuncHist; 0.5*e.'*e];
         
         % update step
         delta = -(A.'*A + 0.5*eye(3)) \ A.' * e;
@@ -38,35 +38,35 @@ function [C_sm, costFuncHist] = calibrateFrames(syncedData, phi, TOL)
     %% Plotting to evaluate performance visually
     accMocap_calibrated = zeros(3,numAcc);
     for lv1=1:1:numAcc
-        accMocap_calibrated(1:3,lv1) = C_sm * syncedData.accMocap(:,lv1);
+        accMocap_calibrated(1:3,lv1) = C_sm * dataSynced.accMocap(:,lv1);
     end
     omegaMocap_calibrated = zeros(3,numGyr);
     for lv1=1:1:numGyr
-        omegaMocap_calibrated(1:3,lv1) = C_sm * syncedData.omegaMocap(:,lv1);
+        omegaMocap_calibrated(1:3,lv1) = C_sm * dataSynced.omegaMocap(:,lv1);
     end
 
     % Plot calibrated data - accelerometers
     figure
     sgtitle('Accelerometers')
     subplot(3,1,1)
-    plot(syncedData.t, accMocap_calibrated(1,:))
+    plot(dataSynced.t, accMocap_calibrated(1,:))
     hold on
-    plot(syncedData.t, syncedData.accIMU(1,:))
+    plot(dataSynced.t, dataSynced.accIMU(1,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\ddot{x}$ [$m/s^2$]', 'Interpreter', 'Latex')
     legend('Calibrated Mocap Data', 'IMU Data')
     subplot(3,1,2)
-    plot(syncedData.t, accMocap_calibrated(2,:))
+    plot(dataSynced.t, accMocap_calibrated(2,:))
     hold on
-    plot(syncedData.t, syncedData.accIMU(2,:))
+    plot(dataSynced.t, dataSynced.accIMU(2,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\ddot{y}$ [$m/s^2$]', 'Interpreter', 'Latex')
     subplot(3,1,3)
-    plot(syncedData.t, accMocap_calibrated(3,:))
+    plot(dataSynced.t, accMocap_calibrated(3,:))
     hold on
-    plot(syncedData.t, syncedData.accIMU(3,:))
+    plot(dataSynced.t, dataSynced.accIMU(3,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\ddot{z}$ [$m/s^2$]', 'Interpreter', 'Latex')
@@ -75,24 +75,24 @@ function [C_sm, costFuncHist] = calibrateFrames(syncedData, phi, TOL)
     figure
     sgtitle('Gyroscopes')
     subplot(3,1,1)
-    plot(syncedData.t, omegaMocap_calibrated(1,:))
+    plot(dataSynced.t, omegaMocap_calibrated(1,:))
     hold on
-    plot(syncedData.t, syncedData.omegaIMU(1,:))
+    plot(dataSynced.t, dataSynced.omegaIMU(1,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\omega_x$ [rad/$s$]', 'Interpreter', 'Latex')
     legend('Calibrated Mocap Data', 'IMU Data')
     subplot(3,1,2)
-    plot(syncedData.t, omegaMocap_calibrated(2,:))
+    plot(dataSynced.t, omegaMocap_calibrated(2,:))
     hold on
-    plot(syncedData.t, syncedData.omegaIMU(2,:))
+    plot(dataSynced.t, dataSynced.omegaIMU(2,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\omega_y$ [rad/$s$]', 'Interpreter', 'Latex')
     subplot(3,1,3)
-    plot(syncedData.t, omegaMocap_calibrated(3,:))
+    plot(dataSynced.t, omegaMocap_calibrated(3,:))
     hold on
-    plot(syncedData.t, syncedData.omegaIMU(3,:))
+    plot(dataSynced.t, dataSynced.omegaIMU(3,:))
     grid
     xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
     ylabel('$\omega_z$ [rad/$s$]', 'Interpreter', 'Latex')
--- mocapIMU_calibration/main.m
+++ mocapIMU_calibration/main.m
@@ -17,74 +17,9 @@ dataIMU = IMU_csv2struct('2020_07_15_trial2_mmagent1_imu_sensorframe_calibration
 % Synchronize the Mocap and IMU data
 dataSynced = syncTime(spline.RigidBody002, dataIMU)
 
-%% Manually check axes alignment
+% Align the frames of the Mocap and IMU data to find an initial DCM
+[C_sm0, dataAligned] = alignFrames(dataSynced);
 
-% Plot synced data - accelerometers
-figure
-sgtitle('Accelerometers')
-subplot(3,1,1)
-plot(dataSynced.t, dataSynced.accMocap(1,:))
-hold on
-plot(dataSynced.t, dataSynced.accIMU(1,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\ddot{x}$ [$m/s^2$]', 'Interpreter', 'Latex')
-legend('Mocap Data', 'IMU Data')
-subplot(3,1,2)
-plot(dataSynced.t, dataSynced.accMocap(2,:))
-hold on
-plot(dataSynced.t, dataSynced.accIMU(2,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\ddot{y}$ [$m/s^2$]', 'Interpreter', 'Latex')
-subplot(3,1,3)
-plot(dataSynced.t, dataSynced.accMocap(3,:))
-hold on
-plot(dataSynced.t, dataSynced.accIMU(3,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\ddot{z}$ [$m/s^2$]', 'Interpreter', 'Latex')
-
-% Plot synced data - gyroscopes
-figure
-sgtitle('Gyroscopes')
-subplot(3,1,1)
-plot(dataSynced.t, dataSynced.omegaMocap(1,:))
-hold on
-plot(dataSynced.t, dataSynced.omegaIMU(1,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\omega_x$ [rad/$s$]', 'Interpreter', 'Latex')
-legend('Mocap Data', 'IMU Data')
-subplot(3,1,2)
-plot(dataSynced.t, dataSynced.omegaMocap(2,:))
-hold on
-plot(dataSynced.t, dataSynced.omegaIMU(2,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\omega_y$ [rad/$s$]', 'Interpreter', 'Latex')
-subplot(3,1,3)
-plot(dataSynced.t, dataSynced.omegaMocap(3,:))
-hold on
-plot(dataSynced.t, dataSynced.omegaIMU(3,:))
-grid
-xlabel('$t$ [$s$]', 'Interpreter', 'Latex')
-ylabel('$\omega_z$ [rad/$s$]', 'Interpreter', 'Latex')
-
-drawnow
-
-input(strcat('Double check the alignment of the axes from the generated plots.',...
-               ' If they are not aligned, please manually align the axes;',...
-               ' otherwise, press enter to proceed.'));
-
-% Aligning the axes based on the plots
-dataSynced.accIMU(1,:) = -dataSynced.accIMU(1,:);
-dataSynced.omegaIMU(2,:) = -dataSynced.omegaIMU(2,:);
-dataSynced.omegaIMU(3,:) = -dataSynced.omegaIMU(3,:);
-
-close all
-
-%% Find the DCM between the two assigned body frames
-
-[C_sm, costFuncHist] = calibrateFrames(dataSynced);
-
+%% Refine the DCM between the two assigned body frames
+phi0 = DCM_TO_ROTVEC(C_sm0);
+[C_sm, costFuncHist] = calibrateFrames(dataAligned, phi0);
