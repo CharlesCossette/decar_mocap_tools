7938039087ea885e402a0761ce0b542b42c85ff7
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2020-07-27 00:39:51 -0400

    Fix bug when there is no missing data

--- mocapIMU_calibration/mocap_csv2struct.m
+++ mocapIMU_calibration/mocap_csv2struct.m
@@ -178,43 +178,43 @@ for lv1=1:1:objectNum
     % Find timesteps where there is missing data.
     tMissing = t(:, ~any(waypointsIter,1));
     
-    % Find the time difference between the datapoints with missing data.
-    tMissingDiff = [tMissing(1), diff(tMissing)];
-    
-    % Find the indices of datapoints where the time difference exceeds a
-    % threshold.
-    tMissingIndices = find(tMissingDiff>thresDiff);
-    if tMissingIndices(end) ~= length(tMissing)
-        tMissingIndices = [tMissingIndices, length(tMissing)];
-    end
-        
     rangeIgnore = [];
-    for lv2=1:1:length(tMissingIndices)-1
-        % check if ll(b) is a solo point or lies within a range of data
+    if ~isempty(tMissing)
+        % Find the time difference between the datapoints with missing data.
+        tMissingDiff = [tMissing(1), diff(tMissing)];
         
-        if length(tMissing) ==79
-            1
-        end
-        
-        if tMissing(tMissingIndices(lv2)+1) - tMissing(tMissingIndices(lv2)) < thresDiff
-            
-            % compute lower limit
-            lower = tMissing(tMissingIndices(lv2)) - bufferSize;
-            if lower < 0; lower = 0; end
-            
-            % compute upperlimit
-            upper = tMissing(tMissingIndices(lv2+1)-1) + bufferSize;
-            
-            % save range
-            if isempty(rangeIgnore)
-                rangeIgnore = [rangeIgnore, [lower; upper]];
-            elseif lower > rangeIgnore(2,end)
-                rangeIgnore = [rangeIgnore, [lower; upper]];
-            else
-                rangeIgnore(2,end) = upper;
+        % Find the indices of datapoints where the time difference exceeds a
+        % threshold.
+        tMissingIndices = find(tMissingDiff>thresDiff);
+        if ~isempty(tMissingIndices)
+            if tMissingIndices(end) ~= length(tMissing)
+                tMissingIndices = [tMissingIndices, length(tMissing)];
+            end
+            for lv2=1:1:length(tMissingIndices)-1
+                % check if solo point or within a range of data
+                if tMissing(tMissingIndices(lv2)+1) - tMissing(tMissingIndices(lv2)) < thresDiff
+
+                    % compute lower limit
+                    lower = tMissing(tMissingIndices(lv2)) - bufferSize;
+                    if lower < 0; lower = 0; end
+
+                    % compute upperlimit
+                    upper = tMissing(tMissingIndices(lv2+1)-1) + bufferSize;
+
+                    % save range
+                    if isempty(rangeIgnore)
+                        rangeIgnore = [rangeIgnore, [lower; upper]];
+                    elseif lower > rangeIgnore(2,end)
+                        rangeIgnore = [rangeIgnore, [lower; upper]];
+                    else
+                        rangeIgnore(2,end) = upper;
+                    end
+                end
             end
         end
     end
+    
+    
     
     S.(objectNames{lv1}).mocapGaps = rangeIgnore;
 end
