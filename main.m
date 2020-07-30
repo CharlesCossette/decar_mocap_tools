c93b0eda09e994909f56f3ebbcdef09541dfb90a
Charles Cossette <charles.cossette@mail.mcgill.ca>
2020-07-30 12:41:43 -0400

    New alignFrames code. Needs a bit more testing

--- main.m
+++ main.m
@@ -49,8 +49,13 @@ dataSynced = syncTime(spline.RigidBody002, dataIMU)
 dataSyncedCleaned = deleteGaps(dataSynced, dataMocap.RigidBody002.mocapGaps)
 
 %% Align the frames of the Mocap and IMU data to find an initial DCM
+tic
+[C2, dataAligned2] = alignFrames2(dataSyncedCleaned)
+toc
+%%
+tic
 [C_sm0, dataAligned] = alignFrames(dataSyncedCleaned)
-
+toc
 %% Refine the DCM between the two assigned body frames
 phi0 = DCM_TO_ROTVEC(C_sm0);
 x0 = [phi0;0;0;0;0;0;0];
