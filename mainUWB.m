7466524df47ab4c9a60cf9a8ec21ebc5df9f9c61
Charles Cossette <charles.cossette@mail.mcgill.ca>
2020-08-07 11:02:10 -0400

    Added a dataset as a test (Natalia 180 slow trial 2020_07_30)

--- mainUWB.m
+++ mainUWB.m
@@ -6,17 +6,17 @@ clear; close all;
 dataMocap = mocap_csv2struct('Mama_Agents_Take_003.csv')
 
 %% Fit a b-spline to the Mocap data
-spline = mocap_fitSpline(dataMocap,[],true)
+splineMocap = mocap_fitSpline(dataMocap,[],true)
 
 %% Extract the IMU data
 [dataIMU, imuFirst_t] = IMU_csv2struct('2020_07_15_trial3_mmagent1_imu.csv')
 
 %% Synchronize the Mocap and IMU data
-[~, offset] = syncTime(spline.RigidBody002, dataIMU);
+[~, offset] = syncTime(splineMocap.RigidBody002, dataIMU);
 
 % Adjust the spline such that it is also synchronized with the IMU/UWB
 % data.
-spline.RigidBody002.knots = spline.RigidBody002.knots - offset;
+splineMocap.RigidBody002.knots = splineMocap.RigidBody002.knots - offset;
 
 %% Extract the UWB data
 % To synchronize the IMU and the UWB files, ensure that "imuFirst_t" comes
@@ -26,13 +26,13 @@ dataUWB = UWB_csv2struct('2020_07_15_trial3_mmagent1_range.csv', imuFirst_t)
 %% Extract position of UWB tags relative to the reference point of the Mocap System
 % r_Ltag2z_b = 0x6a5d, r_Rtag2z_b = 0x6a26, r_Ltag1z_b = 0x6a25, r_Rtag1z_b = 0x6a7b
 [r_Ltag2z_b, r_Rtag2z_b, r_Ltag1z_b, r_Rtag1z_b] = ...
-         extractKeypoints('Tag_Position_Calibration_Take001.csv',...
+         mocap_getPointsInBodyFrame('Tag_Position_Calibration_Take001.csv',...
                           {'RigidBody002', 'RigidBody002', 'RigidBody', 'RigidBody'},...
                           {'Unlabeled1343', 'Unlabeled1342', 'Unlabeled1340', 'Unlabeled1341'});
 
 %% Extract the UWB biases
 [bias_Ltag1z, bias_Rtag1z, bias_Ltag2z, bias_Rtag2z] = ...
-    extractUwbBias(spline, dataUWB,...
+    extractUwbBias(splineMocap, dataUWB,...
                    [r_Ltag1z_b,   r_Rtag1z_b,  r_Ltag2z_b,     r_Rtag2z_b],...
                     {'RigidBody', 'RigidBody', 'RigidBody002', 'RigidBody002'},...
                     {'0x6a25',    '0x6a7b',    '0x6a5d',       '0x6a26'});
