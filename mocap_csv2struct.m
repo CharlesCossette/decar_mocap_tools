cbad602c437b3e4538b9ccb51b80f23302083912
Charles Cossette <charles.cossette@mail.mcgill.ca>
2020-08-11 12:44:46 -0400

    Minor

--- mocap_csv2struct.m
+++ mocap_csv2struct.m
@@ -207,46 +207,7 @@ for lv1=1:1:objectNum
     % Find timesteps where there is missing data.
     isMissing = ~any(waypointsIter,1);
     gapIntervals = getIntervalsFromIndices(t, isMissing, thresDiff, bufferSize);
-%     tMissing = t(:, isMissing);
-%     
-%     gapIntervals = [];
-%     if ~isempty(tMissing)
-%         % Find the time difference between the datapoints with missing data.
-%         tMissingDiff = [tMissing(1), diff(tMissing)];
-%         
-%         % Find the indices of datapoints where the time difference exceeds a
-%         % threshold.
-%         tMissingIndices = find(tMissingDiff>thresDiff);
-%         if ~isempty(tMissingIndices)
-%             if tMissingIndices(end) ~= length(tMissing)
-%                 tMissingIndices = [tMissingIndices, length(tMissing)];
-%             end
-%             for lv2=1:1:length(tMissingIndices)-1
-%                 % check if solo point or within a range of data
-%                 if tMissing(tMissingIndices(lv2)+1) - tMissing(tMissingIndices(lv2)) < thresDiff
-%                     
-%                     % compute lower limit
-%                     lower = tMissing(tMissingIndices(lv2)) - bufferSize;
-%                     if lower < 0; lower = 0; end
-%                     
-%                     % compute upperlimit
-%                     upper = tMissing(tMissingIndices(lv2+1)-1) + bufferSize;
-%                     
-%                     % save range
-%                     if isempty(gapIntervals)
-%                         gapIntervals = [gapIntervals, [lower; upper]];
-%                     elseif lower > gapIntervals(2,end)
-%                         gapIntervals = [gapIntervals, [lower; upper]];
-%                     else
-%                         gapIntervals(2,end) = upper;
-%                     end
-%                 end
-%             end
-%         end
-%     end
-    
-    
-    
+
     S.(objectNames{lv1}).gapIntervals = gapIntervals;
 end
 
