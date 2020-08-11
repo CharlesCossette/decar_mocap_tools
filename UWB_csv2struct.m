ffb4545a107f05ee9ca32c6493406528337e2491
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2020-08-11 15:57:02 -0400

    Generalize the UWB data extraction to accommodate RPI output.

--- UWB_csv2struct.m
+++ UWB_csv2struct.m
@@ -42,6 +42,13 @@ function S = UWB_csv2struct(filename, t0)
     
     % Timestep
     index = find(contains(headers(headerRow,:),'Timestamp'));
+    
+    % If the data is organized side by side, reorganize.
+    if length(index) > 1
+        [data, headers] = horToVer(data, headers, index);
+        index = index(1);
+    end
+    
     S.t = data(:,index);
     % ensure unit is seconds
     if contains(headers(headerRow,index),'(ns)') 
@@ -53,30 +60,67 @@ function S = UWB_csv2struct(filename, t0)
 
     % UWB data
     for lv1=1:1:length(headers)
+        tag = cell(2,1);
         if contains(headers(headerRow,lv1),'Range')
-            % name of first UWB tag
-            tag1 = headers(headerRow-1,lv1);
-            tag1 = tag1{1};
-            
-            % name of second UWB tag
-            temp = headers(headerRow,lv1);
-            temp = temp{1};
-            tagNameIndex = strfind(temp, '0x');
-            tag2 = temp(tagNameIndex:tagNameIndex+5);
-            
+            % extract names of UWB tags
+            headersIter = headers(:,lv1);
+            i=1;
+            for lv2=1:1:length(headersIter)
+                tagIndices = strfind(headersIter{lv2}, '0x');
+                for lv3=1:1:length(tagIndices)
+                    stringIter = headers(lv2,lv1);
+                    stringIter = stringIter{1};
+                    tag{i}  = stringIter(tagIndices(lv3):tagIndices(lv3)+5);
+                    i = i+1;
+                end
+                
+                if i == 3
+                    break
+                end
+            end
+  
             % save 
-            S.(['tags', '_', tag1, '_', tag2]).meas = data(:,lv1);
+            S.(['tags', '_', tag{1}, '_', tag{2}]).meas = data(:,lv1);
             
             % ensure all units are in meters
             if contains(headers(headerRow,lv1),'(mm)') 
-                S.(['tags', '_', tag1, '_', tag2]).meas = S.(['tags', '_', tag1, '_', tag2]).meas/1000;
+                S.(['tags', '_', tag{1}, '_', tag{2}]).meas = S.(['tags', '_', tag{1}, '_', tag{2}]).meas/1000;
             elseif contains(headers(headerRow,lv1),'(cm)') 
-                S.(['tags', '_', tag1, '_', tag2]).meas = S.(['tags', '_', tag1, '_', tag2]).meas/100;
+                S.(['tags', '_', tag{1}, '_', tag{2}]).meas = S.(['tags', '_', tag{1}, '_', tag{2}]).meas/100;
             elseif ~contains(headers(headerRow,lv1),'(m)')
                 warning('Unrecognized range unit.')
             end    
         end
     end
 
+end
+
+function [dataNew, headersNew] = horToVer(data, headers, index)
+    numData = size(data,1);
+    dataNew = data;
+    for lv1=2:1:length(index)
+        % find key columns associated with the current set of data
+        timestepCol = index(lv1);
+        firstCol    = index(lv1)+1;
+        try 
+            lastCol = index(lv1+1)-1;
+        catch
+            lastCol = size(data,2);
+        end
+        
+        % update the data 
+        dataNew = [dataNew(1:end, 1:timestepCol-1), NaN(size(data,1), lastCol-firstCol+1);...
+                   data(1:numData,timestepCol), NaN(numData, timestepCol-index(1)-1), data(1:numData, firstCol:lastCol)];
+    end
+    
+    % Update the headers
+    headersNew = headers;
+    for lv1=length(index):-1:2
+        headersNew(:,index(lv1)) = [];
+    end
+    
+    % sort according to the timestamp
+    [~,idx] = sort(dataNew(:,index(1)));
+    dataNew = dataNew(idx,:);
 end
 
