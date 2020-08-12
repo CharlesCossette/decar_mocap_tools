2a40187b4e7988cf084ca9f33ea8afea83222bba
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2020-08-12 12:05:06 -0400

    Renamed extractUwbBias() to calibrateUWB(). Commented calibrateUWB() and UWB_csv2struct.m

--- UWB_csv2struct.m
+++ UWB_csv2struct.m
@@ -1,14 +1,39 @@
 function S = UWB_csv2struct(filename, t0)
-% Extracts UWB data.
-% The input t0 synchronizes the UWB data with the IMU.
+% UWB_csv2struct extracts the range information from the csv file generated
+% by a RPI or a Pozyx device, and stores it into a struct. 
+
+% WARNING: Requires MATLAB 2019a or later with the AEROSPACE TOOLBOX.
+
+% Inputs:
+% --------
+% filename: [string]
+%       filename of Range raw csv, must be on the matlab path.
+% t0: [double]
+%       the value used to subtract the timestamps of the IMU data collected
+%       on the same device as the range measurements. This allows
+%       synchronization between the UWB data and the IMU data. 
+
+% Outputs:
+% --------
+%   S: [struct] with fields
+%       t: [N x 1] double.
+%           Time points of all the data.
+%       tags_0x####_0x####: [N x 1] double
+%           Range measurements between the tags specified in the field
+%           name.
+    
     
     if nargin < 2
         t0 = 0; % no time synchronization.
     end
 
+    % Use built-in matlab function to automatically detect the header rows.
+    % It is okay to take more rows than the headers, but it is not okay to
+    % take less rows, so a conservative approach is used.
     opts = detectImportOptions(filename, 'Delimiter', ',');
     header_row_range = [num2str(1),':',num2str(opts.DataLine(1)+1)];
 
+    % Store headers into cell array, data into matrix
     headers = readcell(filename, 'Range',header_row_range, 'Delimiter', ',');
     data = readmatrix(filename, 'NumHeaderLines',opts.DataLine(1), 'Delimiter', ',');
 
@@ -44,29 +69,33 @@ function S = UWB_csv2struct(filename, t0)
         end
     end
     
-    % Timestep
+    % Find the columns that contain the timestamps.
     index = find(contains(headers(headerRow,:),'Timestamp'));
     
-    % If the data is organized side by side, reorganize.
+    % If the data is organized side by side, reorganize using horToVer().
     if length(index) > 1
         [data, headers] = horToVer(data, headers, index);
         index = index(1);
     end
     
+    % Extract the timestamps.
     S.t = data(:,index);
-    % ensure unit is seconds
+    
+    % Ensure unit is seconds
     if contains(headers(headerRow,index),'(ns)') 
         S.t = S.t./10^9;        % ns to s
     elseif ~contains(headers(headerRow,index),'(s)')
         warning('Unrecognized time unit.')
     end
-    S.t = S.t - t0; % Synchronize with the IMU data
+    
+    % Synchronize with the IMU data
+    S.t = S.t - t0;
 
-    % UWB data
+    % Extracting the UWB data
     for lv1=1:1:length(headers)
         tag = cell(2,1);
         if contains(headers(headerRow,lv1),'Range')
-            % extract names of UWB tags
+            % Extract names of the 2 UWB tags
             headersIter = headers(:,lv1);
             i=1;
             for lv2=1:1:length(headersIter)
@@ -83,10 +112,10 @@ function S = UWB_csv2struct(filename, t0)
                 end
             end
   
-            % save 
+            % Save to struct
             S.(['tags', '_', tag{1}, '_', tag{2}]) = data(:,lv1);
             
-            % ensure all units are in meters
+            % Ensure all units are in meters
             if contains(headers(headerRow,lv1),'(mm)') 
                 S.(['tags', '_', tag{1}, '_', tag{2}]) = S.(['tags', '_', tag{1}, '_', tag{2}])/1000;
             elseif contains(headers(headerRow,lv1),'(cm)') 
@@ -99,32 +128,37 @@ function S = UWB_csv2struct(filename, t0)
 
 end
 
-function [dataNew, headersNew] = horToVer(data, headers, index)
-    numData = size(data,1);
-    dataNew = data;
+function [dataVer, headers] = horToVer(dataHor, headers, index)
+    numData = size(dataHor,1);
+    dataVer = dataHor;
     for lv1=2:1:length(index)
-        % find key columns associated with the current set of data
+        % Find key columns associated with the current set of data
         timestepCol = index(lv1);
         firstCol    = index(lv1)+1;
         try 
             lastCol = index(lv1+1)-1;
         catch
-            lastCol = size(data,2);
+            lastCol = size(dataHor,2);
         end
         
-        % update the data 
-        dataNew = [dataNew(1:end, 1:timestepCol-1), NaN(size(data,1), lastCol-firstCol+1);...
-                   data(1:numData,timestepCol), NaN(numData, timestepCol-index(1)-1), data(1:numData, firstCol:lastCol)];
+        % Update the data 
+        savedData = dataVer(1:end, 1:timestepCol-1);
+        savedDataNaNCols = NaN(size(dataHor,1), lastCol-firstCol+1);
+        newDataTimestamps = dataHor(1:numData,timestepCol);
+        newDataNaNCols = NaN(numData, timestepCol-index(1)-1);
+        newData = dataHor(1:numData, firstCol:lastCol);
+        
+        dataVer = [savedData, savedDataNaNCols;...
+                   newDataTimestamps, newDataNaNCols, newData];
     end
     
     % Update the headers
-    headersNew = headers;
     for lv1=length(index):-1:2
-        headersNew(:,index(lv1)) = [];
+        headers(:,index(lv1)) = [];
     end
     
-    % sort according to the timestamp
-    [~,idx] = sort(dataNew(:,index(1)));
-    dataNew = dataNew(idx,:);
+    % Sort according to the timestamp
+    [~,idx] = sort(dataVer(:,index(1)));
+    dataVer = dataVer(idx,:);
 end
 
--- calibrateUWB.m
+++ calibrateUWB.m
@@ -1,5 +1,38 @@
-function dataUwbCorrected = extractUwbBias(splineMocap, dataUWB, r_pz_b, bodyNames, tagNames)
-    
+function dataUwbCorrected = calibrateUWB(splineMocap, dataUWB, r_pz_b, bodyNames, tagNames)
+% calibrateUWB corrects the UWB measurements by removing biases using 
+% ground truth data. To do so, a heavily smoothed spline is fit to the
+% error between the range measurements and the ground truth distance, which
+% is then subtracted from the range measurements.
+
+% Inputs:
+% --------
+% splineMocap: [struct]
+%       Contains a bunch of spline parameters of the different rigid bodies
+%       detected by the Mocap system. Used to extract the ground truth
+%       distance between the tags.
+% dataUWB: [struct]
+%       Contains the range measurements between each pair of tags.
+% r_pz_b: [3 x M] double
+%       An array where each column represents the position of a tag
+%       relative to the reference point of the Mocap system.
+% bodyNames: [1 x M] cell
+%       Contains the RigidBody names where each tag lies, in the same tag
+%       order specified by r_pz_b. The names come from the convention used
+%       by the Mocap system.
+% tagNames: [1 x M] cell
+%       Contains the serial code of each tag, in the same order specified
+%       by r_pz_b. 
+
+% Outputs:
+% --------
+%   dataUwbCorrected: [struct] with fields
+%       tags_0x####_0x####: [struct] with fields
+%           t: [N x 1] double.
+%              Time points in which measurements were collected between
+%              these two tags.
+%           meas: [N x 1] double.
+%              The measurements between the two tags.
+
     % initialize struct to store new corrected data
     dataUwbCorrected = struct();
 
