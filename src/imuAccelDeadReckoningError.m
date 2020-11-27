b27eda6eebcbe846f67f296128c9da59fd86b612
Charles Cossette <charles.cossette@mail.mcgill.ca>
2020-11-27 11:31:35 -0500

    Minor changes to imu dead reckoning error functions

--- src/imuAccelDeadReckoningError.m
+++ src/imuAccelDeadReckoningError.m
@@ -31,54 +31,56 @@ function [err, error_position, error_velocity, error_accel] = ...
     error_velocity = nan(3,length(dataSynced.t));
     error_accel = nan(3,length(dataSynced.t));
     for lv1 = start_index:interval_size:end_index 
-        if (lv1 + batch_size) > end_index
-            N = end_index - lv1 + 1;
-        else
-            N = batch_size;
+        if ~is_gap(lv1)
+            if (lv1 + batch_size) > end_index
+                N = end_index - lv1 + 1;
+            else
+                N = batch_size;
+            end
+            idx = lv1:lv1 + N -1;
+            idx = idx(:);
+            isStaticInBatch = idx(:).*is_static(idx);
+            isStaticInBatch = isStaticInBatch(isStaticInBatch ~= 0);
+
+            % Initial conditions from mocap (if we detected static, force zero
+            % velocity).
+            r_zw_a_0 = dataSynced.r_zw_a(:,lv1);
+            if is_static(lv1)
+                v_zwa_a_0 = zeros(3,1);
+            else
+                v_zwa_a_0 = dataSynced.v_zwa_a(:,lv1);
+            end
+            t_span = dataSynced.t(idx);        
+
+            % Initialize
+            N = length(t_span);
+            r_zw_a_dr = zeros(3,N);
+            v_zwa_a_dr = zeros(3,N);
+            a_zwa_a_dr = zeros(3,N);
+            r_zw_a_dr(:,1) = r_zw_a_0;
+            v_zwa_a_dr(:,1) = v_zwa_a_0;
+
+            % Dead reckoning        
+            for lv2 = 1:N-1
+                dt = t_span(lv2+1) - t_span(lv2);
+
+                % Use zero-order hold
+                measurement_index = find(data_corrected.t <= t_span(lv2), 1, 'last'); 
+                u_acc_b = data_corrected.accel(:,measurement_index);
+                C_ba = dataSynced.C_ba(:,:,measurement_index); % TODO CHECK INDEX.
+                v_zwa_a_dr(:,lv2+1) = v_zwa_a_dr(:,lv2) + (C_ba.'*u_acc_b + g_a)*dt;
+                r_zw_a_dr(:,lv2+1) = r_zw_a_dr(:,lv2) + v_zwa_a_dr(:,lv2)*dt;
+                a_zwa_a_dr(:,lv2) = C_ba.'*u_acc_b + g_a;
+            end
+
+            % Build errors. If static, force zero velocity as the reference. 
+            error_position(:,idx) = dataSynced.r_zw_a(:,idx) - r_zw_a_dr;
+            error_velocity(:,idx) = dataSynced.v_zwa_a(:,idx) - v_zwa_a_dr;                                   
+            error_accel(:,idx) = dataSynced.a_zwa_a(:,idx) - a_zwa_a_dr;
+
+            error_velocity(:,isStaticInBatch) = -v_zwa_a_dr(:,isStaticInBatch - lv1 + 1);
+            error_accel(:,isStaticInBatch) = -a_zwa_a_dr(:,isStaticInBatch - lv1 + 1);
         end
-        idx = lv1:lv1 + N -1;
-        idx = idx(:);
-        isStaticInBatch = idx(:).*is_static(idx);
-        isStaticInBatch = isStaticInBatch(isStaticInBatch ~= 0);
-        
-        % Initial conditions from mocap (if we detected static, force zero
-        % velocity).
-        r_zw_a_0 = dataSynced.r_zw_a(:,lv1);
-        if is_static(lv1)
-            v_zwa_a_0 = zeros(3,1);
-        else
-            v_zwa_a_0 = dataSynced.v_zwa_a(:,lv1);
-        end
-        t_span = dataSynced.t(idx);        
-        
-        % Initialize
-        N = length(t_span);
-        r_zw_a_dr = zeros(3,N);
-        v_zwa_a_dr = zeros(3,N);
-        a_zwa_a_dr = zeros(3,N);
-        r_zw_a_dr(:,1) = r_zw_a_0;
-        v_zwa_a_dr(:,1) = v_zwa_a_0;
-        
-        % Dead reckoning        
-        for lv2 = 1:N-1
-            dt = t_span(lv2+1) - t_span(lv2);
-            
-            % Use zero-order hold
-            measurement_index = find(data_corrected.t <= t_span(lv2), 1, 'last'); 
-            u_acc_b = data_corrected.accel(:,measurement_index);
-            C_ba = dataSynced.C_ba(:,:,measurement_index); % TODO CHECK INDEX.
-            v_zwa_a_dr(:,lv2+1) = v_zwa_a_dr(:,lv2) + (C_ba.'*u_acc_b + g_a)*dt;
-            r_zw_a_dr(:,lv2+1) = r_zw_a_dr(:,lv2) + v_zwa_a_dr(:,lv2)*dt;
-            a_zwa_a_dr(:,lv2) = C_ba.'*u_acc_b + g_a;
-        end
-                               
-        % Build errors. If static, force zero velocity as the reference. 
-        error_position(:,idx) = dataSynced.r_zw_a(:,idx) - r_zw_a_dr;
-        error_velocity(:,idx) = dataSynced.v_zwa_a(:,idx) - v_zwa_a_dr;                                   
-        error_accel(:,idx) = dataSynced.a_zwa_a(:,idx) - a_zwa_a_dr;
-        
-        error_velocity(:,isStaticInBatch) = -v_zwa_a_dr(:,isStaticInBatch - lv1 + 1);
-        error_accel(:,isStaticInBatch) = -a_zwa_a_dr(:,isStaticInBatch - lv1 + 1);
     end
     
     % Add weight when static. 
