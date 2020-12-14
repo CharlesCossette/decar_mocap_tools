e9fcdc5a290d59c32b767cf10809961e32ccc0dd
Charles Cossette <charles.cossette@mail.mcgill.ca>
2020-12-14 10:27:56 -0500

    main.m file now runs with new functions

--- deprecated/calibrateImu.m
+++ deprecated/calibrateImu.m
@@ -1,5 +1,6 @@
 function [results, data_calibrated] = calibrateImu(data_synced, options, import_results)
 %calibrateImu [DEPRECATED]
+if ~exist('import_results','var')
+    import_results = struct();
+end
 [results, data_calibrated] = imuCalibrate(data_synced, options, import_results)
-
-
